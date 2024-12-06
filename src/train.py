8448ea28b5035eff47f97fef7e0f67fb700d48d0
RichardCernansky <richard.cernansky3@gmail.com>
2024-12-06 22:02:23 +0100

    resarch and getting relevant code from examples

--- src/train.py
+++ src/train.py
@@ -1,72 +1,128 @@
-# Path: src/train.py
-import os
-import yaml
+from typing import List, Optional
+
+import hydra
+import onnx
 import torch
-import pytorch_lightning as pl
-from torch.utils.data import DataLoader
-from src.datamodules.dataset import SegmentationDataset
-from src.models.unet_model import get_unet_model
-from src.losses.loss import get_loss_function
-from src.metrics.metrics import iou_metric
-from src.utils.helpers import save_model
+from omegaconf import DictConfig
+from onnxsim import simplify
+from pytorch_lightning import Callback, LightningDataModule, LightningModule, Trainer, seed_everything
+from lightning.pytorch.loggers.logger import Logger
 
-class SegmentationModel(pl.LightningModule):
-    def __init__(self, model, loss_fn, learning_rate):
-        super().__init__()
-        self.model = model
-        self.loss_fn = loss_fn
-        self.learning_rate = learning_rate
 
-    def forward(self, x):
-        return self.model(x)
+from src.utils import utils
 
-    def training_step(self, batch, batch_idx):
-        images, masks = batch
-        preds = self(images)
-        loss = self.loss_fn(preds, masks)
-        self.log("train_loss", loss)
-        return loss
+log = utils.get_logger(__name__)
 
-    def validation_step(self, batch, batch_idx):
-        images, masks = batch
-        preds = self(images)
-        loss = self.loss_fn(preds, masks)
-        iou = iou_metric(preds, masks)
-        self.log("val_loss", loss)
-        self.log("val_iou", iou)
 
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+def train(config: DictConfig) -> Optional[float]:
+    """Contains training pipeline.
+    Instantiates all PyTorch Lightning objects from config.
+    Args:
+        config (DictConfig): Configuration composed by Hydra.
+    Returns:
+        Optional[float]: Metric score for hyperparameter optimization.
+    """
 
-def main():
-    # Load config
-    with open("configs/config_segment.yaml", "r") as f:
-        config = yaml.safe_load(f)
+    # Set seed for random number generators in pytorch, numpy and python.random
+    if config.get("seed"):
+        seed_everything(config.seed, workers=True)
 
-    # Prepare data
-    train_dataset = SegmentationDataset(
-        config["data"]["train_images"],
-        config["data"]["train_masks"],
-        config["training"]["img_size"]
+    # Init lightning datamodule
+    log.info(f"Instantiating datamodule <{config.datamodule._target_}>")
+    datamodule: LightningDataModule = hydra.utils.instantiate(config.datamodule)
+
+    # Init lightning model
+    log.info(f"Instantiating model <{config.model._target_}>")
+    model: LightningModule = hydra.utils.instantiate(
+        config.model)
+
+    # Init lightning callbacks
+    callbacks: List[Callback] = []
+    if "callbacks" in config:
+        for _, cb_conf in config.callbacks.items():
+            if "_target_" in cb_conf:
+                log.info(f"Instantiating callback <{cb_conf._target_}>")
+                callbacks.append(hydra.utils.instantiate(cb_conf))
+
+    # Init lightning loggers
+    logger: List[Logger] = []
+    if "logger" in config:
+        for _, lg_conf in config.logger.items():
+            if "_target_" in lg_conf:
+                log.info(f"Instantiating logger <{lg_conf._target_}>")
+                logger.append(hydra.utils.instantiate(lg_conf))
+
+    # Init lightning trainer
+    log.info(f"Instantiating trainer <{config.trainer._target_}>")
+    trainer: Trainer = hydra.utils.instantiate(
+        config.trainer, callbacks=callbacks, logger=logger, _convert_="partial"
     )
-    val_dataset = SegmentationDataset(
-        config["data"]["val_images"],
-        config["data"]["val_masks"],
-        config["training"]["img_size"]
+
+    # Send some parameters from config to all lightning loggers
+    log.info("Logging hyperparameters!")
+    utils.log_hyperparameters(
+        config=config,
+        model=model,
+        datamodule=datamodule,
+        trainer=trainer,
+        callbacks=callbacks,
+        logger=logger,
     )
-    train_loader = DataLoader(train_dataset, batch_size=config["training"]["batch_size"], shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=config["training"]["batch_size"])
 
-    # Prepare model, loss, and trainer
-    model = get_unet_model(encoder_name=config["training"]["model_name"])
-    loss_fn = get_loss_function()
-    seg_model = SegmentationModel(model, loss_fn, config["training"]["learning_rate"])
+    # Train the model
+    log.info("Starting training!")
+    if not config.eval_mode:
+        trainer.fit(model=model, datamodule=datamodule)
 
-    trainer = pl.Trainer(max_epochs=config["training"]["epochs"], accelerator="gpu" if torch.cuda.is_available() else "cpu")
-    trainer.fit(seg_model, train_loader, val_loader)
+    # Evaluate model on test set, using the best model achieved during training
+    if config.get("test_after_training"):
+        log.info("Starting testing!")
+        if config.eval_mode:
+            trainer.test(model=model, datamodule=datamodule,
+                         ckpt_path=config.trainer.resume_from_checkpoint)
+        else:
+            trainer.test(model=model, datamodule=datamodule, ckpt_path='best')
 
-    # Save model
-    save_model(seg_model.model, config["output"]["model_path"])
+    # Make sure everything closed properly
+    log.info("Finalizing!")
+    utils.finish(
+        config=config,
+        model=model,
+        datamodule=datamodule,
+        trainer=trainer,
+        callbacks=callbacks,
+        logger=logger,
+    )
 
-if __name__ == "__main__":
-    main()
+
+    # Print path to best checkpoint
+    if not config.eval_mode:
+        log.info(f"Best model ckpt: {trainer.checkpoint_callback.best_model_path}")
+
+    if config.get("export").get('export_to_onnx'):
+        opset = config.get("export").get('opset')
+        use_simplifier = config.get("export").get('use_simplifier')
+        log.info(f'Export model to onnx! Params: opset={opset}, use_simplifier={use_simplifier}')
+
+        model.eval()
+        x = next(iter(datamodule.test_dataloader()))[0]
+
+        torch.onnx.export(model.network,
+                          x[:1],  # model input (or a tuple for multiple inputs)
+                          'model.onnx',  # where to save the model (can be a file or file-like object)
+                          export_params=True,  # store the trained parameter weights inside the model file
+                          opset_version=opset,  # the ONNX version to export the model to
+                          input_names=['input'],
+                          output_names=['output'],
+                          do_constant_folding=False)
+
+        if use_simplifier:
+            model = onnx.load('model.onnx')
+            model_simp, check = simplify(model)
+            assert check, "Simplified ONNX model could not be validated"
+            onnx.save(model, 'model.onnx')
+
+    # Return metric score for hyperparameter optimization
+    optimized_metric = config.get("optimized_metric")
+    if optimized_metric:
+        return trainer.callback_metrics[optimized_metric]
